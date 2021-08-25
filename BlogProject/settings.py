54c8bc87d8ebd059b4648da4e5bc0b045cc882df
Nema Chandra Goswami <83756388+Nym-Git@users.noreply.github.com>
2021-08-26 04:25:16 +0530

    update

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: "-+gz=ghuf1)gvr#4loli$v@($=*yxt)h2&_st+$hqt865mf!1o" keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '-+gz=ghuf1)gvr#4loli$v@($=*yxt)h2&_st+$hqt865mf!1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
