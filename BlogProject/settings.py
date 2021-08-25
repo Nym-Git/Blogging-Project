05ded84d94b7f7ab3f9cbecfa0a93c7aaf1aef8a
Nema Chandra Goswami <83756388+Nym-Git@users.noreply.github.com>
2021-08-26 04:22:12 +0530

    update for heroku

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',           #just for heroku
+    #'whitenoise.middleware.WhiteNoiseMiddleware',           #just for heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'        #just for heroku   [pip install whitenoise]
+#STATIC_ROOT = BASE_DIR / 'static'        #just for heroku   [pip install whitenoise]
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/Blogging/APP')
